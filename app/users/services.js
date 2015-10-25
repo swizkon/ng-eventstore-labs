f8f587425c0f41f14ca9e4e8db727cadb785f262
Jonas Jerndin <jonas.jerndin@ne.se>
2015-10-25 20:13:19 +0100

    Some new stuff

---
+++ app/users/services.js
@@ -0,0 +1,29 @@
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+var eventstore = {
+  	
+	  appendToStream: function(streamName, eventType, eventBody, callback){
+		  var eventData = [
+			  {
+			      "eventId": generateUUID(),
+			      "eventType": eventType,
+			      "data": eventBody
+			  }
+		  ];
+		  $.ajax({
+		      url: "http://localhost:2113/streams/" + streamName,
+		      type: "POST",
+		      data: JSON.stringify(eventData),
+		      contentType: "application/vnd.eventstore.event+json",
+		      complete: callback
+		  });
+	  }
+  };
